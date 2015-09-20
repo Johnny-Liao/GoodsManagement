83770ddca00c8fee7b8b19f9c04757e795733e7c
Daniel Chiu <1360874697@qq.com>
2015-09-20 15:28:11 +0800

    封装dao

--- src/main/java/org/goodsManagement/dao/BasedaoA.java
+++ src/main/java/org/goodsManagement/dao/BasedaoA.java
@@ -3,14 +3,8 @@ package org.goodsManagement.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
 /**
- * 本类是DAO工具类，定义了dao基本的操作接口，并且支持进行复杂的hql查询
- * 支持分页查询
- * 之所以将很多本来可以在这里实现的CRUD操作交给子类重新实现是因为子类很有可能在CRUD之前进行检查工作
- * 每个实体类的检查时各不相同的，所以还是讲方法设计为抽象类
- * Created by never on 2015/8/25.
+ * Created by never on 2015/9/20.
  */
 public abstract class BasedaoA<T> {
 
@@ -18,120 +12,50 @@ public abstract class BasedaoA<T> {
     protected SqlSessionTemplate sessionTemplate;
 
     /**
-     * 添加实体
+     * 通过主键删除
      *
-     * @param t 实体对象
-     */
-    public abstract void addEntity(T t);
-
-    /**
-     * 修改实体
-     *
-     * @param t 实体对象
-     */
-    public abstract void modifyEntity(T t);
-
-    /**
-     * load具有懒加载效果，获得实体类
-     *
-     * @param id 实体对象的id
+     * @param id
      * @return
      */
-    public abstract T loadEntity(int id);
+    public abstract int deleteByPrimaryKey(Integer id);
 
     /**
-     * 获得实体类
+     * 插入记录，必须指定所有的字段
      *
-     * @param id 实体对象的id
+     * @param record
      * @return
      */
-    public abstract T getEntity(int id);
+    public abstract int insert(T record);
 
     /**
-     * 获得所有的实体类
+     * 插入记录，必须指定非空的字段，但是其他字段可选
      *
+     * @param record
      * @return
      */
-    public abstract List<T> getAllEntities();
-
+    public abstract int insertSelective(T record);
 
     /**
-     * 删除指定的某个实体
+     * 通过主键获取实体
      *
-     * @param t 实体对象
+     * @param id
      * @return
      */
-    public abstract void deleteEntity(T t);
+    public abstract T selectByPrimaryKey(Integer id);
 
     /**
-     * 使用hql语言进行查询操作
+     * 更新实体类，不需要指定所有的键
      *
-     * @param hql hql语句
+     * @param record
      * @return
      */
-//    public List<?> find(String hql) {
-//        Query q = sessionTemplate.getSqlSessionFactory().openSession()..getSessionFactory().getCurrentSession().createQuery(hql);
-//        return q.list();
-//    }
+    public abstract int updateByPrimaryKeySelective(T record);
 
     /**
-     * 提供Map对应的传参hql查询
-     * <p>
-     * from table t where t.id =:id and t.name=:name
-     * map中应该这样添加
-     * map.put("id",Id);
-     * map.put("name",Name);
+     * 通过主键来更新实体类
      *
-     * @param hql    hql语句
-     * @param params 参数集
-     * @return hql语句的结果集
+     * @param record
+     * @return
      */
-//    public List<?> find(String hql, Map<String, Object> params) {
-//        Query q = hibernateTemplate.getSessionFactory().getCurrentSession().createQuery(hql);
-//        if (params != null && !params.isEmpty()) {
-//            for (String key : params.keySet()) {
-//                q.setParameter(key, params.get(key));
-//            }
-//        }
-//        return q.list();
-//    }
-
-    /**
-     * 支持分页查询提供Map对应的传参hql查询
-     * <p>
-     * from table t where t.id =:id and t.name=:name
-     * map中应该这样添加
-     * map.put("id",Id);
-     * map.put("name",Name);
-     *
-     * @param hql    hql语句
-     * @param params 参数集
-     * @param page   分页中页数
-     * @param rows   每页中的行数
-     * @return hql语句的结果集
-     */
-//    public List<?> find(String hql, Map<String, Object> params, int page, int rows) {
-//        Query q = hibernateTemplate.getSessionFactory().getCurrentSession().createQuery(hql);
-//        if (params != null && !params.isEmpty()) {
-//            for (String key : params.keySet()) {
-//                q.setParameter(key, params.get(key));
-//            }
-//        }
-//        return q.setFirstResult((page - 1) * rows).setMaxResults(rows).list();
-//    }
-
-    /**
-     * 支持分页查询hql查询
-     * <p>
-     * from table t where t.id =:id and t.name=:name
-     *
-     * @param hql  hql语句
-     * @param page 分页中页数
-     * @param rows 每页中的行数
-     * @return hql语句的结果集
-     */
-//    public List<?> find(String hql, int page, int rows) {
-//        Query q = hibernateTemplate.getSessionFactory().getCurrentSession().createQuery(hql);
-//        return q.setFirstResult((page - 1) * rows).setMaxResults(rows).list();
-//    }
+    public abstract int updateByPrimaryKey(T record);
 }
---
+++ src/main/java/org/goodsManagement/dao/impl/DeptdaoImpl.java
@@ -0,0 +1,76 @@
+package org.goodsManagement.dao.impl;
+
+import org.goodsManagement.dao.BasedaoA;
+import org.goodsManagement.po.DeptDto;
+
+/**
+ * Created by never on 2015/9/20.
+ */
+public class DeptdaoImpl<T> extends BasedaoA<DeptDto> {
+
+    /**
+     * 通过主键删除
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.deleteByPrimaryKey", id);
+    }
+
+    /**
+     * 插入记录，必须指定所有的字段
+     *
+     * @param record
+     * @return
+     */
+    @Override
+    public int insert(DeptDto record) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.insert", record);
+    }
+
+    /**
+     * 插入记录，必须指定非空的字段，但是其他字段可选
+     *
+     * @param record
+     * @return
+     */
+    @Override
+    public int insertSelective(DeptDto record) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.insertSelective", record);
+    }
+
+    /**
+     * 通过主键获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public DeptDto selectByPrimaryKey(Integer id) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.selectByPrimaryKey", id);
+    }
+
+    /**
+     * 更新实体类，不需要指定所有的键
+     *
+     * @param record
+     * @return
+     */
+    @Override
+    public int updateByPrimaryKeySelective(DeptDto record) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.updateByPrimaryKeySelective", record);
+    }
+
+    /**
+     * 通过主键来更新实体类
+     *
+     * @param record
+     * @return
+     */
+    @Override
+    public int updateByPrimaryKey(DeptDto record) {
+        return sessionTemplate.selectOne("org.goodsManagement.mapper.DeptDtoMapper.updateByPrimaryKey", record);
+    }
+}
