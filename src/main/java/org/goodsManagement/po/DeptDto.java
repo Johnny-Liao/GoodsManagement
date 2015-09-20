aac53883d25d029f9ce3c83bf99d52023e1a452f
Daniel Chiu <1360874697@qq.com>
2015-09-20 19:57:15 +0800

    modified entity

--- src/main/java/org/goodsManagement/po/DeptDto.java
+++ src/main/java/org/goodsManagement/po/DeptDto.java
@@ -1,60 +1,22 @@
 package org.goodsManagement.po;
 
 public class DeptDto {
-    /**
-     * This field corresponds to the database column dept.id
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     private Integer id;
 
-    /**
-     * This field corresponds to the database column dept.deptname
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     private String deptname;
 
-    /**
-     * This method returns the value of the database column dept.id
-     *
-     * @return the value of dept.id
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     public Integer getId() {
         return id;
     }
 
-    /**
-     * This method sets the value of the database column dept.id
-     *
-     * @param id the value for dept.id
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * This method returns the value of the database column dept.deptname
-     *
-     * @return the value of dept.deptname
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     public String getDeptname() {
         return deptname;
     }
 
-    /**
-     * This method sets the value of the database column dept.deptname
-     *
-     * @param deptname the value for dept.deptname
-     *
-     * @mbggenerated Sun Sep 20 14:45:05 CST 2015
-     */
     public void setDeptname(String deptname) {
         this.deptname = deptname == null ? null : deptname.trim();
     }
