835c63255b5deb80130e76c13db4987d8d709efe
sunshine-life <904470788@qq.com>
2015-09-23 11:20:37 +0800

    add getgoodstoexcel

--- src/main/java/org/goodsManagement/service/impl/PoiUtils/GetGoodsToExcel.java
+++ src/main/java/org/goodsManagement/service/impl/PoiUtils/GetGoodsToExcel.java
@@ -3,6 +3,7 @@ package org.goodsManagement.service.impl.PoiUtils;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.goodsManagement.po.GetGoodsDto;
+import org.goodsManagement.vo.GetGoodsVO;
 
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class GetGoodsToExcel {
      *          要写入的文件的路径
      */
 
-    public void toExcel(List<GetGoodsDto> list,String path){
+    public void toExcel(List<GetGoodsVO> list,String path){
 
         HSSFWorkbook wb = new HSSFWorkbook();
         HSSFSheet sheet = wb.createSheet("Users");
@@ -30,8 +31,11 @@ public class GetGoodsToExcel {
             value[0][m] = n[m];
         }
         for (int i = 0; i < list.size(); i++) {
-            GetGoodsDto  getGoodsDto= (GetGoodsDto) list.get(i);
-            value[i + 1][0] = getGoodsDto.getId();
+            GetGoodsVO  getGoodsVOg= (GetGoodsVO) list.get(i);
+            value[i + 1][0] = getGoodsVOg.getStaffname();
+            value[i + 1][1] = getGoodsVOg.getGoodname();
+            value[i + 1][2] = getGoodsVOg.getGoodtype();
+            value[i + 1][3] = getGoodsVOg.getGetnumber();
         }
         ExcelUtils.writeArrayToExcel(wb, sheet, list.size() + 1, 4, value);
         ExcelUtils.writeWorkbook(wb, path);
