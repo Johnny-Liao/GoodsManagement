d7f819537c5b82631debbaba762fe5ac300b132b
sunshine-life <904470788@qq.com>
2015-09-24 11:33:16 +0800

    nothing

--- src/main/java/org/goodsManagement/controller/InRepositoryAction.java
+++ src/main/java/org/goodsManagement/controller/InRepositoryAction.java
@@ -8,6 +8,7 @@ import org.goodsManagement.po.InRepositoryDto;
 import org.goodsManagement.service.impl.InRepositoryServiceImpl;
 import org.goodsManagement.vo.Inrepositorysql;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 
 import java.util.List;
 
@@ -15,51 +16,58 @@ import java.util.List;
  * Created by lifei on 2015/9/22.
  */
 //@ParentPackage("struts-default")
-@Namespace("/")
-@Action(value = "inRepository",
+//@Namespace("/")
+@Scope("prototype")
+/*@Action(value = "inRepository",
         results = {@Result(name = "success", location = "/sussess.jsp"),
-                @Result(name = "error", location = "/sussess.jsp")})
+                @Result(name = "error", location = "/sussess.jsp")})*/
 public class InRepositoryAction extends ActionSupport {
-    private String filename;
-    private List<InRepositoryDto> list;
-
-    public List<InRepositoryDto> getList() {
-        return list;
+    public InRepositoryAction(){
+        System.out.println("action被实例化");
     }
-
-    public void setList(List<InRepositoryDto> list) {
-        this.list = list;
-    }
-
-    @Autowired
-    private InRepositoryServiceImpl inRepositoryServiceImpl;
+//    private String filename;
+//    private List<InRepositoryDto> list;
+//
+//    public List<InRepositoryDto> getList() {
+//        return list;
+//    }
+//
+//    public void setList(List<InRepositoryDto> list) {
+//        this.list = list;
+//    }
+//
+//    @Autowired
+//    private InRepositoryServiceImpl inRepositoryServiceImpl;
 
     /**
      * 用来读取文件并写入数据库
      */
-    public String add() {
+    /*public String add() {
         System.out.println(filename);
         inRepositoryServiceImpl.addinRepositoryD(filename);
+
+        System.out.println("进入到了action");
         return SUCCESS;
-    }
+    }*/
 
     public String getAll() {
-        System.out.println(filename);
-        list = inRepositoryServiceImpl.selectallmes();
-        return SUCCESS;
+//        System.out.println(filename);
+//        list = inRepositoryServiceImpl.selectallmes();
+        System.out.println("拿到所有信息的方法");
+        return "getAll";
     }
-
-    public String selectbysearch() {
-        Inrepositorysql sql = new Inrepositorysql();
-        list = inRepositoryServiceImpl.selectbysearch(sql);
-        return SUCCESS;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
+//
+//    public String selectbysearch() {
+//        Inrepositorysql sql = new Inrepositorysql();
+//        list = inRepositoryServiceImpl.selectbysearch(sql);
+//        return SUCCESS;
+//    }
+//
+//    public String getFilename() {
+//        return filename;
+//    }
+//
+//    public void setFilename(String filename) {
+//        this.filename = filename;
+//    }
 }
