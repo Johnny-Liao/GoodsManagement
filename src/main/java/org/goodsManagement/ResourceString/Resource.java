72ea134280858cf6991cb3d533ccce817dac5977
Daniel Chiu <1360874697@qq.com>
2015-09-26 14:15:56 +0800

    add package-info

--- src/main/java/org/goodsManagement/ResourceString/Resource.java
+++ src/main/java/org/goodsManagement/ResourceString/Resource.java
@@ -3,14 +3,14 @@ package org.goodsManagement.ResourceString;
 /**
  * Created by lifei on 2015/9/24.
  */
-public class Resource {
+public final class Resource {
     /**
      * 入库单表头
      */
-    public static String[] InrepositoryHead = {"入库单号","到货时间","供应商","联系人","联系电话","编制人","操作"};
+    public final static String[] InrepositoryHead = {"入库单号", "到货时间", "供应商", "联系人", "联系电话", "编制人", "操作"};
     /**
      * 入库单货物表头
      */
-    public static String[] InrepositoryGoodHead = {"入库物品","单位","型号","数量"};
+    public final static String[] InrepositoryGoodHead = {"入库物品", "单位", "型号", "数量"};
 
 }
---
+++ src/main/java/org/goodsManagement/po/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 本包中类主要作为MyBatis中的实体映射类
+ * <ol>
+ * <li>po是Persistence Object的缩写，表示持久化对象，从整体架构来看，它们确实是ORM中的持久化对象</li>
+ * <li>dto是data transport Object的缩写，表示数据传输对象，从程序运行的角度，它们确实是数据传输对象</li>
+ * <li>以下是对这个包中的类的介绍</li>
+ * <ol>
+ *      <li>DeptDto表示部门实体对象</li>
+ *      <li>DeptDto表示部门实体对象</li>
+ * </ol>
+ * </ol>
+ * Created by never on 2015/9/26.
+ */
+package org.goodsManagement.po;
